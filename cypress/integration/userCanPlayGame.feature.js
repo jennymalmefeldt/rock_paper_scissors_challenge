ebc75282a00eb78e146c2e7bcebad77ef181cd05
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-21 19:55:37 +0200

    Changing the feature test

--- cypress/integration/userCanPlayGame.feature.js
+++ cypress/integration/userCanPlayGame.feature.js
@@ -1,7 +1,6 @@
-describe('user can '), () => {
-  beforeEach(() =>
-    it('displays the content of Rock'), () =>
-      cy.visit('/');
-      cy.get('button#rock').click();
-      cy.get('#message').should('contain', 'You win!');
-)};
+describe('user can', () => {
+  it('play game', () => {
+    cy.visit("/");
+    cy.get('button#rock').click();
+    cy.get('#message').should('contain', 'You win');
+  });
--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rock: "win"
-      paper: "it's a draw"
+      rock: "win",
+      paper: "it's a draw",
       scissor: "lose"
     };
   }
   changeResult = () => {
-    this.setState({result: });
+    this.setState({result: "rock" });
   }
   render() {
     return
     <div>
       <h1> User {this.state.rock}</h1>
+      <p>
+        It is a {this.state.paper}
+      </p>
       <button
       type="button"
         onClick={this.changeResult}>
