0c144df7ad1a9b95dd322ae3fe620319ddf77aeb
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-22 15:03:06 +0200

    Added button 'Play Game' and and function to fore the user to see whos the winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,61 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rock: "win",
-      paper: "it's a draw",
-      scissor: "lose"
+class App extends React.Component {
+    state = {
+      userChoice: '',
+      winner: ''
     };
+
+  chooseUserWeapon = (event) => {
+    this.setState({userChoice: event.target.name})
   }
-  changeResult = () => {
-    this.setState({rock: "lose" });
+
+  playGame = async () => {
+    let userWeapon = this.state.userChoice
+    let computerWeapon = 'rock'
+    debugger
+    let winner = await this.theWinnerIs(userWeapon, computerWeapon)
+    debugger
+    this.setState({winner: winner})
+  }
+
+  theWinnerIs = (userWeapon, computerWeapon) => {
+if (userWeapon !== computerWeapon) {
+  return 'USER WINS!'
+} else {
+  return 'ITS A TIE!'
+}
   }
   render() {
     return ( 
       <div>
-      <h1> User {this.state.rock}</h1>
-      <h2>
-        User {this.state.rock} 
-      </h2>
+     
+      {/* <h2>
+        User {this.props.rock} 
+      </h2> */}
       <button
       id="button"
-        onClick={this.changeRock}>
+      name='rock'
+        onClick={this.chooseUserWeapon}>
           Rock
       </button>
       <button
       id="button"
-        onClick={this.changePaper}>
+      name='paper'
+        onClick={this.chooseUserWeapon}>
           Paper
       </button>
       <button
       id="button"
-        onClick={this.changeScissor}>
+      name='scissor'
+        onClick={this.chooseUserWeapon}>
           Scissor
       </button>
+      <h1> User chose {this.state.userChoice}</h1>
+      <button
+      onClick={this.playGame}>Play Game!</button>
 
+      <h1>{this.state.winner}</h1>
     </div>
     )
   }
