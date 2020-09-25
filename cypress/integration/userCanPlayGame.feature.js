fac32bdafd4328f5453ec7bcc4f74b1dfe38c976
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-25 09:53:30 +0200

    Modifying test

--- cypress/integration/userCanPlayGame.feature.js
+++ cypress/integration/userCanPlayGame.feature.js
@@ -1,21 +1,26 @@
 describe("User can choose a weapon", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.get('button#play-game').should('contain', 'Play Game!')
+  });
+
+  describe('User tab and it', () => {
+    beforeEach(() => {
+      cy.get('button#play-game').click();
+    })
   });
 
   it('User choose rock', () => {
     cy.get('button#rock').click();
-    cy.get('#message').should('contain', 'Choose you weapon!: You picked rock')
+    cy.get('#message').should('contain', 'Choose you weapon! You picked rock')
   });
 
   it('User choose paper', () => {
     cy.get('button#paper').click();
-    cy.get('#message').should('contain', 'Choose you weapon!: You picked paper')
+    cy.get('#message').should('contain', 'Choose you weapon! You picked paper')
   })
 
   it('User choose scissor', () => {
     cy.get('button#scissor').click();
-    cy.get('#message').should('contain', 'Choose you weapon!: You picked scissor')
+    cy.get('#message').should('contain', 'Choose you weapon! You picked scissor')
   })
 });
